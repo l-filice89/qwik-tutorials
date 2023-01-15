03d3c9002881f914e787293e6e9021f2b46f23ba
Luca Filice <l.filice.89@gmail.com>
2023-01-15 09:30:33 +0100

    Basic component

--- qwik-getting-started/src/routes/index.tsx
+++ qwik-getting-started/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import {component$} from '@builder.io/qwik';
 import type {DocumentHead} from '@builder.io/qwik-city';
+import {Link} from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <div>
       <h1>
-        Qwik tutorials
+        Qwik tutorial components!
       </h1>
+      <ul>
+        <li>
+          <Link href="/basic-component">Basic component</Link>
+        </li>
+      </ul>
     </div>
   );
 });
